8a604d303165fd948932f9569416d07a1df898a5
Will <wshagen3@gmail.com>
2025-08-20 23:08:51 -0700

    trying a new slider.tsx

--- frontend/src/components/ui/Slider.tsx
+++ frontend/src/components/ui/Slider.tsx
@@ -1,7 +1,6 @@
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import { cn } from "@/lib/utils";
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -15,12 +14,25 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    {/* Track */}
+    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-[color:var(--primary)/0.2]">
+      {/* Range */}
+      <SliderPrimitive.Range className="absolute h-full bg-[color:var(--primary)]" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
 
-export { Slider }
+    {/* Thumb */}
+    <SliderPrimitive.Thumb className="
+      block h-4 w-4 rounded-full 
+      border border-[color:var(--primary)/0.5] 
+      bg-[color:var(--background)] 
+      shadow transition-colors 
+      focus-visible:outline-none 
+      focus-visible:ring-1 focus-visible:ring-[color:var(--ring)] 
+      disabled:pointer-events-none disabled:opacity-50
+    " />
+  </SliderPrimitive.Root>
+));
+
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };
