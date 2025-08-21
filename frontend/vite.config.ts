6428ed2d26bdd123aa902e050af87fc4d2ccdf8c
Will <wshagen3@gmail.com>
2025-08-20 20:34:53 -0700

    x

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 3000,
-	},
-
-	css: {
-		postcss: './postcss.config.cjs',
-	}
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 });
