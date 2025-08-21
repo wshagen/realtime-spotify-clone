895696521107f35f2f159a7fb70253c676ea0d3e
Will <wshagen3@gmail.com>
2025-08-20 20:03:08 -0700

    rearranged some files after checking source code

--- backend/src/server.js
+++
@@ -1,4 +0,0 @@
-delete process.env.DEBUG_URL;
-console.log("ENV:", process.env);
-
-import './index.js';
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,15 @@
-import { fileURLToPath } from "url";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// ESM-compatible __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 3000,
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		port: 3000,
+	},
 });
